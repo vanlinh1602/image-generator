faa40cef3e6c1b80b100ac17213a362cc27f2685
Nguyễn Văn Linh <linhnv1622@gmail.com>
2024-10-29 13:34:51 +0700

    update code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,12 +2,14 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import Together from 'together-ai';
 
+import { API_KEY } from '@/configs/key';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 const together = new Together({
-  apiKey: '2244e78ace3efd0c45e4a1739efb5a3f230b4e6127e806991ae999c62ffd114f',
+  apiKey: API_KEY,
 });
 
 export const generateImage = async (
